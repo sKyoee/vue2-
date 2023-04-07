36a8a99e40df3ac3f180087580da2a2e81faa223
sKyoe <1552889375@qq.com>
2023-04-07 18:54:29 +0800

    创建mvList组件

---
+++ src/components/List/MVList.vue
@@ -0,0 +1,26 @@
+<template></template>
+
+<script>
+import { get } from "../../request/index";
+export default {
+  props: {
+    id: {
+      type: Number,
+      required: true,
+    },
+  },
+  methods: {
+    async getArtistMv(id) {
+        const res = await get('/artist/mv',{id})
+        if(res.code !== 200) return
+        console.log(res)
+    },
+  },
+  created() {
+    this.getArtistMv(this.id)
+  },
+};
+</script>
+
+<style>
+</style>
